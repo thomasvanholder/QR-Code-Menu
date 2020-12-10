bc49bbd9b04f39ef9bad9a1e95856a90ae7f5721
Thomas Van Holder <thomasvanholder@hey.com>
2020-12-10 13:20:17 +0100

    up

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,25 @@
+class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_menu, only: [:new]
+
+  def new
+    @category = @menu.categories.new
+  end
+
+  def create
+    @category = @menu.categories.build(category_params)
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:menu_id])
+  end
+
+  def category_params
+    require(:category).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :menus
+  get 'categories/new'
+  get 'categories/edit'
+  resources :menus do
+    resources :categories, shallow: true
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
